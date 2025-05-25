296fe6ffb35c12bb2ac56eeb1547822fa48bdb63
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-05-25 15:46:54 -0400

    Added Base Title and Scoreboard Sections

---
+++ src/components/TitleHeader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TitleHeader = () => {
+  return (
+    <div>
+      <h1 className="title">Pokemon Memory Card</h1>
+      <h2 className="subtitle">
+        Click on a Pokemon to earn points, but don't click on any more than
+        once!
+      </h2>
+    </div>
+  );
+};
+
+export default TitleHeader;
